2d1853bc03fd6d0b8b71a8e1464066f8a4b5c32a
Alexander Pérez Ruiz <alexander.perez@escuelaing.edu.co>
2025-08-26 09:25:55 -0500

    Agregando un programa que valida tamaños de tipos de dato.

---
+++ C_C++/tipos.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+
+int main(){
+    cout << "Tamano del bool " << sizeof(bool) << endl;
+    cout << "Tamano del char " << sizeof(char) << endl;
+    cout << "Tamano del short " << sizeof(short) << endl;
+    cout << "Tamano del int " << sizeof(int) << endl;
+    cout << "Tamano del long " << sizeof(long) << endl;
+    cout << "Tamano del long long " << sizeof(long long) << endl;
+    cout << "Tamano del float " << sizeof(float) << endl;
+    cout << "Tamano del double " << sizeof(double) << endl;
+    cout << "Tamano del long double " << sizeof(long double) << endl;
+    return 0;
+}
