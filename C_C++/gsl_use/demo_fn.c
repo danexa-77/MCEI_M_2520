d72b235ee0acef0f6b1f6f4897f6bb052e546a31
Alexander Pérez Ruiz <alexander.perez@escuelaing.edu.co>
2025-09-09 10:22:07 -0500

    Usando la GSL con CMake.

---
+++ C_C++/gsl_use/demo_fn.c
@@ -0,0 +1,28 @@
+#include "demo_fn.h"
+
+double quadratic (double x, void *params)
+{
+  quadratic_params *p = ( quadratic_params *) params;
+  double a = p->a;
+  double b = p->b;
+  double c = p->c;
+  return (a * x + b) * x + c;
+}
+
+double quadratic_deriv (double x, void *params)
+{
+  quadratic_params *p = (quadratic_params *) params;
+  double a = p->a;
+  double b = p->b;
+  return 2.0 * a * x + b;
+}
+
+void quadratic_fdf (double x, void *params, double *y, double *dy)
+{
+  quadratic_params *p = ( quadratic_params *) params;
+  double a = p->a;
+  double b = p->b;
+  double c = p->c;
+  *y = (a * x + b) * x + c;
+  *dy = 2.0 * a * x + b;
+}
---
+++ C_C++/gsl_use/demo_fn.h
@@ -0,0 +1,12 @@
+#ifndef DEMO_FN_H
+#define DEMO_FN_H
+
+struct quadratic_params{
+   double a, b, c;
+};
+
+double quadratic (double x, void *params);
+double quadratic_deriv (double x, void *params);
+void quadratic_fdf (double x, void *params, double *y, double *dy);
+
+#endif
